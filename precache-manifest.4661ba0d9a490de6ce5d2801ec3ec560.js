d99f807da3bf2039db9157a0bb24d34e96beab20
Casey Banks <casey.banks@theoaklandgroup.co.uk>
2020-11-13 15:18:34 +0000

    Updates

---
+++ precache-manifest.4661ba0d9a490de6ce5d2801ec3ec560.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "96cd44a07d5baa9fe153",
+    "url": "/endOf020/static/css/main.e07e3d3f.chunk.css"
+  },
+  {
+    "revision": "96cd44a07d5baa9fe153",
+    "url": "/endOf020/static/js/main.96cd44a0.chunk.js"
+  },
+  {
+    "revision": "61f864938b386815688c",
+    "url": "/endOf020/static/js/runtime~main.61f86493.js"
+  },
+  {
+    "revision": "314e80c93f74d23ccf9d",
+    "url": "/endOf020/static/js/2.314e80c9.chunk.js"
+  },
+  {
+    "revision": "fe7c8ebc272e909bda6c9c6717d306ba",
+    "url": "/endOf020/index.html"
+  }
+];
